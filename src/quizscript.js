ff0206ceea5cf2ccf62742f5b23122de9fcd6144
knutbo <knutbo@7e765cbf-b741-0410-971a-5d4381fa7f3e>
2007-12-28 11:39:27 +0000

    New first page logo Formatted html files New question.

--- src/quizscript.js
+++ src/quizscript.js
@@ -1,18 +1,18 @@
 function loadNextPage() {
-   document.location = pageToLoad;
+    document.location = pageToLoad;
 }
 
 function doPuff(nextpage) {
-   pageToLoad = nextpage;
-   new Effect.Puff('everything', { afterFinish:loadNextPage });
+    pageToLoad = nextpage;
+    new Effect.Puff('everything', { afterFinish:loadNextPage });
 }
 
 
 function doNext(nextpage) {
-   pageToLoad = nextpage;
-   new Effect.DropOut('everything', { afterFinish:loadNextPage });
+    pageToLoad = nextpage;
+    new Effect.DropOut('everything', { afterFinish:loadNextPage });
 }
 
-function zoom(div) {  
-  Element.setContentZoom(div, 150); 
+function zoom(div) {
+    Element.setContentZoom(div, 150);
 }
